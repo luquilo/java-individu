87588d85fe7e568a9fc0b357549caa889ce0eeb3
luquilo <luquilo.etc@gmail.com>
2023-06-01 17:33:33 +0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        String nama = "muhammad luqmanul hakim";
+        int nim = 2113010276;
+
+        String pesan = String.format("\nhalo, nama saya %s, dan nim saya %d", nama, nim);
+        
+
+        System.out.println(pesan);
+        
+
+        
+    
+    }
+}
